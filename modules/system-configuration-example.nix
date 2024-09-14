abe04e872d8df546489c53566cdb6cfaa9a8558f
arsmoriendy <nugrahasteven@gmail.com>
2024-09-14 10:33:44 +0700

    feat: wg-quick template

--- modules/system-configuration-example.nix
+++ modules/system-configuration-example.nix
@@ -10,4 +10,19 @@
   environment.etc."crypttab".text = ''
     Data /dev/nvme0n1p7 none tcrypt-veracrypt,password-echo=masked
   '';
+
+  networking.wg-quick.interfaces.wg0 = {
+    autostart = false;
+    address = ["10.0.0.2/24" "fc00::2/7"];
+    privateKeyFile = "/media/Data/Documents/wireguard_keys/wg0_private";
+    peers = [
+      {
+        publicKey = "yeEZCclVIn8QS8fmNt2jZ9nh76M/NNJbD4aFGjXYMQk=";
+        allowedIPs = ["0.0.0.0/0" "::/0"];
+        endpoint = "arsmoriendy.com:57621";
+        persistentKeepalive = 25;
+      }
+    ];
+  };
+
 }
